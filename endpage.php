4cd445bed37934d7037a388b5f9efc7ccf18eaf4
Sravani0802 <104447346+Sravani0802@users.noreply.github.com>
2022-04-26 21:15:43 +0530

    Add files via upload

---
+++ endpage.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<style >
+		*{
+		box-sizing: border-box;
+	}
+	body{
+		background-image:url("lo-bck.jpeg") ;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	#ex{
+		text-align: center;
+		font-weight: 40%;
+		margin-top: 10%;
+	}
+	</style>
+</head>
+<body>
+	<div id="ex">
+		<p>Session ended ,Please Login again.</p>
+		<a href="login.php" >Ok</a>
+	</div>
+</body
+</html
